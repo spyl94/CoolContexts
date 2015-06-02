a2298fccb12df130a8e746d05e890a2b44e99e6b
Aurélien David <adavid@jolicode.com>
2015-06-02 23:04:31 +0200

    here it begins

---
+++ README.md
@@ -0,0 +1,8 @@
+Since I use these contexts in every projects, I thought it would be nice to have them in a separate repo...
+
+Here they are : **CoolContexts** !
+
+They are highly coupled to my projects, but you can find some inspiration there.
+Be nice with them and create cool things ;-)
+
+Build with love by @spyl94.
---
+++ src/Spyl/CoolContexts/DefaultContext.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Spyl\CoolContexts;
+
+use Behat\Symfony2Extension\Context\KernelDictionary;
+use Behat\Symfony2Extension\Context\KernelAwareContext;
+
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+abstract class DefaultContext implements KernelAwareContext
+{
+    use KernelDictionary;
+
+    /**
+     * Get entity manager.
+     *
+     * @return ObjectManager
+     */
+    protected function getEntityManager()
+    {
+        return $this->getService('doctrine')->getManager();
+    }
+
+    /**
+     * Get service by id.
+     *
+     * @param string $id
+     *
+     * @return object
+     */
+    protected function getService($id)
+    {
+        return $this->getContainer()->get($id);
+    }
+
+    /**
+     * Get parameter by id.
+     *
+     * @param string $id
+     *
+     * @return object
+     */
+    protected function getParameter($id)
+    {
+        return $this->getContainer()->getParameter($id);
+    }
+
+    /**
+     * Get current user instance.
+     *
+     * @return null|UserInterface
+     *
+     * @throws \Exception
+     */
+    protected function getUser()
+    {
+        $token = $this->getService('security.token_storage')->getToken();
+
+        if (null === $token) {
+            throw new \Exception('No token found in token storage.');
+        }
+
+        return $token->getUser();
+    }
+
+    /**
+     * @BeforeSuite
+     */
+    public static function reinitDatabase()
+    {
+        DefaultContext::databaseContainsFixtures();
+    }
+
+    /**
+     * @AfterScenario @database
+     */
+    public static function databaseContainsFixtures()
+    {
+        exec('php app/console doctrine:fixtures:load -n -e test');
+    }
+}
